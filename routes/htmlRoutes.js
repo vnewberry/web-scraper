f83de798e0c762d8656cc128f72af15818d1e08d
Vance Newberry <vancenewberry@gmail.com>
2020-06-15 23:32:04 -0500

    routing index

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,9 @@
+// var db = require("../models");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+  
+      res.render("index");
+    
+  })}
---
+++ server.js
@@ -0,0 +1,48 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// Scraping tools
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Requiring all models
+// const db = require("./models");
+
+// Initializing the port
+const PORT = process.env.PORT || 3000;
+
+// Initializing Express
+const app = express();
+
+// Middleware
+    // Use morgan logger for logging requests
+    app.use(logger("dev"));
+    // Parse request body as JSON
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    // Make public a static folder
+    app.use(express.static("public"));
+
+// Using Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "/views/layouts")
+}));
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "handlebars");
+
+// require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+// Connecting to the Mongo DB
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/marketWatchScraper";
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function () {
+    console.log('App running on http://localhost:' + PORT + '/');
+});
