31c862785767fd1b254d06df5e341828c3a30dee
Vance Newberry <vancenewberry@gmail.com>
2020-06-16 15:07:14 -0500

    added some styling and organization to articles

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,9 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#article-container").append(`<img src ='${data[i].URL}' />`);
+      let newArticle = $(`<div id='article'></div>`);
+      newArticle.append( `<img class='article-image' src='${data[i].URL}' alt=''><ul><li>${data[i]._id}</li><li>${data[i].headline}</li></ul>`)
+      $("#article-container").append(newArticle);
     }
   });
  
